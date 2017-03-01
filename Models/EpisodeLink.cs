e353645312f2db0d57ac54ac1da2b837d62514ca
Lộc Nguyễn <Lc Nguyn>
2017-03-01 18:35:56 +0700

    safe

---
+++ Models/EpisodeLink.cs
@@ -0,0 +1,22 @@
+namespace Entertainment.Models
+{
+    public class EpisodeLink
+    {
+        public string id
+        {
+            get; set;
+        }
+        public string src
+        {
+            get; set;
+        }
+        public string type
+        {
+            get; set;
+        }
+        public string quality
+        {
+            get; set;
+        }
+    }
+}
